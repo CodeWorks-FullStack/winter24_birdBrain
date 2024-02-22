09bee47972543feed3d6f02d4e220891ccf3458d
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-22 12:06:48 -0700

    storing birds in appstate, drawing html template

--- client/app/controllers/BirdsController.js
+++ client/app/controllers/BirdsController.js
@@ -1,8 +1,21 @@
+import { AppState } from "../AppState.js";
 import { birdsService } from "../services/BirdsService.js";
 import { Pop } from "../utils/Pop.js";
+import { setHTML } from "../utils/Writer.js";
+
+function _drawBirds() {
+  const birds = AppState.birds
+  let htmlString = ''
+  birds.forEach(bird => htmlString += bird.BirdCardHTMLTemplate)
+  setHTML('birdCards', htmlString)
+}
+
 
 export class BirdsController {
   constructor () {
+    AppState.on('birds', _drawBirds)
+
+
     console.log('BIRDS CONTROLLER LOADED');
     // Pop.success('You are so good at coding, jeremy + class')
     this.getBirds()
--- client/app/services/BirdsService.js
+++ client/app/services/BirdsService.js
@@ -1,9 +1,14 @@
+import { AppState } from "../AppState.js";
+import { Bird } from "../models/Bird.js";
 import { api } from "./AxiosService.js"
 
 class BirdsService {
   async getBirds() {
     const response = await api.get('api/birds')
     console.log('📡 GOT BIRDS', response.data);
+    const newBirds = response.data.map(birdPOJO => new Bird(birdPOJO))
+    AppState.birds = newBirds
+    // console.log(AppState.birds);
   }
 
 }
